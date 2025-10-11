2bcccda8554b48a97b5b385f427969c5e8b12b0b
FunTom <vogeltanz@utb.cz>
2025-10-11 11:30:46 +0200

    Areas, ProductController, Select View

---
+++ UTB.eshop25.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.eshop25.Web.Areas.Admin.Controllers
+{
+    [Area(nameof(Admin))]
+    public class ProductController : Controller
+    {
+        public IActionResult Select()
+        {
+            return View();
+        }
+    }
+}
