d714931deff2c3df29fe547860376f99d44c4324
FunTom <vogeltanz@utb.cz>
2025-10-11 10:41:23 +0200

    1. migrace Product table

--- UTB.eshop25.Domain/Entities/Product.cs
+++ UTB.eshop25.Domain/Entities/Product.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UTB.eshop25.Domain.Entities
 {
+    [Table(nameof(Product))]
     public class Product : Entity<int>
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        [Required]
+        [StringLength(70)]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        [Range(1, double.MaxValue)]
         public double Price { get; set; }
-        public string ImageSrc { get; set; }
+        public string? ImageSrc { get; set; }
     }
 }
---
+++ UTB.eshop25.Infrastructure/Migrations/20251011083802_mysql_1.0.0_init.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UTB.eshop25.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mysql_100_init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(70)", maxLength: 70, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<double>(type: "double", nullable: false),
+                    ImageSrc = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
